4f8d149ebf26e526dfd9eaf8db828659e4ca4fcc
Aruwyn <budisuryawan96@gmail.com>
2020-03-04 22:48:24 +0800

    Electron Part 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const electron = require("electron");
 const {
     app,
     BrowserWindow,
-    MEnu,
+    Menu,
     ipcMain
 } = electron;
 
@@ -23,5 +23,67 @@ app.on("ready", () => {
 
         app.quit();
         todayWindow = null;
-    })
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appoinment"
+    });
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null));
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appoinment"
+    });
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null));
+};
+
+const menuTemplate = [{
+    label: "File",
+    submenu: [{
+        label: "New Appointment",
+
+        click() {
+            createWindowCreator();
+        }
+    },
+    {
+        label: "All Appoinment",
+        click() {
+            listWindowCreator();
+        }
+    },
+    {
+        label: "Quit",
+        accelerator: process.platform === "darwin" ? "Command + Q" : "CTRL + Q",
+        click() {
+            app.quit();
+        }
+    }
+    ]
+},
+
+{
+    label: "View",
+    submenu: [{ role: "reload" }, { role: "toggledevtools" }]
+}
+]
